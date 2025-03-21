151a74900f3c1f2a2fd0cfdb5031c38fb9300fd7
Edinho <git@final04.com>
2025-03-21 20:27:37 -0300

    wip

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Dockerfile
+
+# pull the official docker image
+FROM python:3.13.2-bookworm
+
+# set work directory
+WORKDIR /app
+
+# set env variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# install dependencies
+COPY pyproject.toml /app/
+COPY uv.lock /app/
+RUN pip install uv
+RUN uv sync --frozen --no-dev
+
+# copy project
+COPY logging.conf /app/
+COPY alembic.ini /app/
+COPY run-server.py /app/
+COPY fabriquinha /app/fabriquinha
---
+++ run-server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Inicia o servidor da aplicação no backend.
+"""
+
+import logging.config
+
+import alembic.config
+import uvicorn
+
+import fabriquinha as fabr
+
+
+logging.config.fileConfig('logging.conf')
+
+
+if __name__ == '__main__':
+    # aplicar migrações
+    alembic_args = ['--raiseerr', 'upgrade', 'head']
+    alembic.config.main(argv=alembic_args)
+
+    # executar o servidor
+    uvicorn.run(fabr.main.app, host='0.0.0.0', port=8000)
