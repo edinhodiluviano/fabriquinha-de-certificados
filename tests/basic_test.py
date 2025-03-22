48faf1897e29eb96f5351d65243ee0f24bdc14b0
Edinho <git@final04.com>
2025-03-22 11:40:34 -0300

    test: adiciona teste para rota de validação

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -21,3 +21,16 @@ def test_criar_config_levanta_erro_com_log_level_errado():
 def test_get_ping_retorna_200(cliente):
     resp = cliente.get('ping')
     assert resp.status_code == 200
+
+
+def test_get_validar_com_codigo_inexistente(certificados, cliente):
+    resp = cliente.get('v/aaaaaaaaaaa')
+    assert resp.status_code == 200
+    assert 'não encontrado' in resp.text
+
+
+def test_get_validar_com_codigo_valido(certificados, cliente):
+    resp = cliente.get('v/' + certificados[0].codigo)
+    assert resp.status_code == 200
+    assert certificados[0].modelo.emissora in resp.text
+    assert 'Certificado OK!' in resp.text
